debdb5b7834f130e22aad58ba3a631182ea4f039
Christopher Klein <ckl@neos-it.de>
2019-08-29 15:39:39 +0200

    [added] support for GITHUB_REF

--- git-meta/entrypoint.sh
+++ git-meta/entrypoint.sh
@@ -7,7 +7,16 @@ GIT_META_DIRECTORY=".git-meta"
 mkdir -p $GIT_META_DIRECTORY
 
 echo `git rev-parse HEAD` > $GIT_META_DIRECTORY/hash
-echo `git branch | grep \* | cut -d ' ' -f2` > $GIT_META_DIRECTORY/branch
+
+BRANCH_INFO=`git status -b -s`
+
+if [ ! -z "$GITHUB_REF" ]; then
+	# if GITHUB_REF is used, assume we are in GH actions and we are probably in a detached state (due to actions/checkout)
+	BRANCH_INFO=$GITHUB_REF
+fi
+# get the last part of of the branch info (e.g. "master...origin/master" for git status or "refs/heads/master") and extract the last portion
+# @see https://stackoverflow.com/a/22727211
+echo `echo $BRANCH_INFO | rev | cut -d'/' -f 1 | rev` > $GIT_META_DIRECTORY/branch
 
 GIT_LAST_MESSAGE=$(git log -1 --pretty=oneline)
 echo $GIT_LAST_MESSAGE > $GIT_META_DIRECTORY/message_short
